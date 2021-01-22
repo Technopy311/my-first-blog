aac10836ef878b7c1527615aa9e738b492353d30
technopy <xmenmagnet@gmail.com>
2021-01-21 22:24:16 -0300

    Following the django-girls tut, starting deploying process

---
+++ myenv/status.py
@@ -0,0 +1,19 @@
+import requests
+#MY USERNAME
+username = 'technopy'
+
+#API TOKEN
+token = '10b2d887a451278ab41eb24924dee71cfbc00093'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+                        
